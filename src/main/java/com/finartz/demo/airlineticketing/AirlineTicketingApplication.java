ae9ff57b4668567c0f450653411032a00952cede
yasin_tulumen <yasin.tulumen@joker.games>
2020-07-14 00:26:38 +0300

    Initial commit

---
+++ src/main/java/com/finartz/demo/airlineticketing/AirlineTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.finartz.demo.airlineticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineTicketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finartz/demo/airlineticketing/AirlineTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finartz.demo.airlineticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
